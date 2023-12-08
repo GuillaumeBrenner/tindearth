75dfae680f29fe6928d2551d56ac5c0bd0c53c45
YaWock <yael.wojcik@ensemblescolaire-niort.com>
2023-12-08 01:56:29 +0100

    Committing local changes before merge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React,  { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState(75);
+  const [maxScore, setMaxScore] = useState(100); 
+  const percentage = (score/maxScore) * 100;
+
+  const increaseScore = () => {
+    // You can add validation logic or constraints if needed
+    setScore(score + 5);
+  };
+
+  const decreaseScore = () => {
+    // You can add validation logic or constraints if needed
+    setScore(score - 5);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>Score Percentage Calculator</h2>
+          <p>Score: {score}</p>
+          <p>Maximum Possible Score: {maxScore}</p>
+          <p>Percentage: {percentage}%</p>
+        </div>
+        <button onClick={increaseScore}>Increase Score</button>
+        <button onClick={decreaseScore}>Increase Score</button>
       </header>
     </div>
   );
